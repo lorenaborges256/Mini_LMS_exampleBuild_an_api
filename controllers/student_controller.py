477abac499bd6b948ee6f4775ef4ac044453c94d
lorenaborges256 <lorena.borges.amaral@gmail.com>
2025-10-29 20:59:32 +1100

    feat: add student controllers

--- controllers/student_controller.py
+++ controllers/student_controller.py
@@ -1,12 +1,28 @@
-from flask import Blueprint
+from flask import Blueprint, jsonify
 from init import db
-from models.student import Student
+from models.student import Student, student_schema, students_schema
+
 
 students_bp = Blueprint('students', __name__, url_prefix='/students')
 
 # Our routes to be defined ....
 
 # GET/students
+
+@students_bp.route('/')
+def get_students():
+    #first define a statement: Select * from students;
+    stmt = db.select(Student)
+    #execute it
+    students_list = db.session.scalars(stmt)
+    #serialise it
+    data = students_schema.dump(students_list)
+    #return it as json, the jsonify(list)
+    if data:
+        return jsonify(data)
+    else:
+        return {"message": "No record found. Add a student to get started with system..."}
+
 # GET/students/id
 # POST/students
 # PUT/PATCH /students/id
