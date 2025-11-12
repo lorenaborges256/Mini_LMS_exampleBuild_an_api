13eaa2275dc7bc9514f0d2f5e93e33a08d14c782
lorenaborges256 <lorena.borges.amaral@gmail.com>
2025-11-12 15:42:22 +1100

    feature: added course schema

---
+++ models/course.py
@@ -0,0 +1,23 @@
+from init import db
+from marshmallow_sqlalchemy import SQLAlchemyAutoSchema
+
+class Course(db.Model):
+    __tablename__ = 'courses'
+    course_id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(100), nullable=False, unique=True)
+    duration = db.Column(db.String(100), nullable=False)
+    
+
+    #foreign key Addition / connecting teachers table here
+    teacher_id = db.Column(db.Integer, db.ForeignKey('teachers.teacher_id'), nullable=False)
+
+#Course Schema
+class CourseSchema(SQLAlchemyAutoSchema):
+    class Meta:
+        model = Course
+        load_instance = True
+        incluse_fk = True
+
+#object defined
+course_schema = CourseSchema()
+courses_schema = CourseSchema(many=True)
