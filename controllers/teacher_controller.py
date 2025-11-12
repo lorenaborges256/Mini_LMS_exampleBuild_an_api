290f865bcb2823862c6e3652be30895ceccd0b68
lorenaborges256 <lorena.borges.amaral@gmail.com>
2025-11-12 14:53:26 +1100

    feat: added DELETE /teachers_controller

--- controllers/teacher_controller.py
+++ controllers/teacher_controller.py
@@ -40,77 +40,94 @@ def get_teachers_byid(teacher_id):
         return {"message": f"Teacher with id: {teacher_id} does not exist!"}
     
 
-# # POST /students
-# @teachers_bp.route("/", methods=["POST"])
-# def create_students():
-#     #get details from request body
-#     body_data = request.get_json()
-#     #create student object with the request body data
-#     email = body_data.get("email")
-#     stmt = db.select(Teacher).where(Teacher.email == email)
-#     #adding to the session
-#     student = db.session.scalar(stmt)
-#     #checking the system with an email add 
-#     data = student_schema.dump(student)
+# POST /teachers
+@teachers_bp.route("/", methods=["POST"])
+def create_teachers():
+    #get details from request body
+    body_data = request.get_json()
+    #create teacher object with the request body data
+    # email = body_data.get("email")
+    stmt = db.select(Teacher)
+    #adding to the session
+    teacher = db.session.scalar(stmt)
+    #checking the system with an email add 
+    data = teacher_schema.dump(teacher)
 
-#     #display message with the same email issue
-#     if data: 
-#         return {"message": f"The Teacher with email:{email} already exists."}
+    #display message with the same email issue
+    # if data: 
+    #     return {"message": f"The Teacher with email:{email} already exists."}
 
-#     new_student = Teacher(
-#         name = body_data.get("name"),
-#         email = body_data.get("email"),
-#         address = body_data.get("address")
-#     )
-#     #add to the section
-#     db.session.add(new_student)
-#     #commit the session
-#     db.session.commit()
-#     #send Ack
-#     #creating a new variable to hold the serialised data
-#     data = student_schema.dump(new_student)
-#     #calling data to convert with jsonify
-#     return jsonify(data) , 201
-# # # Types of error
-# # fields can not be empty
-# # email must be unique
-# # defaults errors - unexpected errors occured
-# # 404 - not found
+    new_teacher = Teacher(
+        name = body_data.get("name"),
+        department = body_data.get("department"),
+        address = body_data.get("address")
+    )
+    #add to the section
+    db.session.add(new_teacher)
+    #commit the session
+    db.session.commit()
+    #send Ack
+    #creating a new variable to hold the serialised data
+    data = teacher_schema.dump(new_teacher)
+    #calling data to convert with jsonify
+    return jsonify(data) , 201
+# # Types of error
+# fields can not be empty
+# email must be unique
+# defaults errors - unexpected errors occured
+# 404 - not found
 
 
-# # DELETE /students/id
-# @teachers_bp.route("/<int:student_id>", methods=["DELETE"])
-# def delete_student(student_id):
-#     #find the student by id: select * from students where student_id=student_id;
-#     stmt = db.select(Teacher).where(Teacher.student_id == student_id)
-#     student = db.session.scalar(stmt)
+# DELETE /teacher/id
+@teachers_bp.route("/<int:teacher_id>", methods=["DELETE"])
+def delete_teacher(teacher_id):
+    #find the teacher by id: select * from teacher where teacher_id=teacher_id;
+    stmt = db.select(Teacher).where(Teacher.teacher_id == teacher_id)
+    teacher = db.session.scalar(stmt)
 
-#     #if student exists:
-#     if student:
-#         #delete
-#         name = student.name
-#         db.session.delete(student)
-#         #commit
-#         db.session.commit()
-#         #return ack
-#         return {"message": f"Teacher with name {student.name} has been deleted successfully."},200
+    #if teacher exists:
+    if teacher:
+        #delete
+        name = teacher.name
+        db.session.delete(teacher)
+        #commit
+        db.session.commit()
+        #return ack
+        return {"message": f"Teacher with name {teacher.name} has been deleted successfully."},200
 
-#     else: 
-#         #return ack no record found
-#         return {"message": f"Teacher with id {student_id} does not exist."}, 404
+    else: 
+        #return ack no record found
+        return {"message": f"Teacher with id {teacher_id} does not exist."}, 404
      
 
-# # PUT/PATCH /students/id (EDIT the student details)
-# @teachers_bp.route("/<int:student_id>", methods=["PUT", "PATCH"])
-# def edit_student(student_id):
-#     #Get the std from db first
-#     #define statemnet
-#     #exc the stmt
-    
-#     #if the std exists
-#         #fetch the info from the request body
-#         # make the changes
-#         #commit to the db
-#         # ack
-#     #else
-#         #
+# PUT/PATCH /teachers/id (EDIT the teaqcher details)
+@teachers_bp.route("/<int:teacher_id>", methods=["PUT", "PATCH"])
+def edit_teacher(teacher_id):
+    # try:
+        # Get the student from db first
+        stmt = db.select(Teacher).where(Teacher.teacher_id == teacher_id)
+        #exc the stmt
+        teacher = db.session.scalar(stmt)
+        
+        #if the std exists
+        if teacher:
+            #fetch the info from the request body
+            body_data = request.get_json()
+            
+            # make the changes (using a short circuit method) using both put and patch
+            # where put updates everything 
+            # patch updates specific key values
+            teacher.name = body_data.get("name", teacher.name)
+            teacher.department = body_data.get("department", teacher.department)
+            teacher.address = body_data.get("address", teacher.address)
+            
+            #commit to the db
+            db.session.commit()
+            # ack
+            return jsonify(teacher_schema.dump(teacher))
+        #else
+        else:
+            return {"message": f"Teacher with id: {teacher_id} does not exist"}, 404
+            # ack 
+    # except IntegrityError as err:
+    #     return {"Email address is already in Use "}, 409
